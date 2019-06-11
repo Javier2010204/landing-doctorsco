89d8617208aa08688cd6498d4ee8a5e070b7e347
Javier2010204 <2010204javiergomez@gmail.com>
2019-06-11 10:47:05 -0600

    efecto en seccion de pacientes

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$(".numbers").boxLoader({
+	    direction:"x",
+	    position: "100%",
+	    effect: "fadeIn",
+	    duration: "1s",
+	    windowarea: "50%"
+	}); 
+})
