5f60cfd3e67919fbc712417af2f5f66d11037895
Javier2010204 <2010204javiergomez@gmail.com>
2019-06-11 18:10:10 -0600

    efecto de inicio

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+	$("#welcome-text").boxLoader({
+		direction: "y",
+		position: "-100%",
+		effect: "fadeIn",
+		duration: "1s",
+		windowarea: "50%"
+	});
+
 	$(".numero1").boxLoader({
 	    direction:"x",
 	    position: "100%",
@@ -29,25 +37,5 @@ $(document).ready(function(){
 	    effect: "fadeIn",
 	    duration: "1s",
 	    windowarea: "50%"
-	}); 
-
-	$.jScrollability([
-            {
-                'selector': '.text-wrapper',
-                'start': function($el) { return $el.offset().top + $el.height() },
-                'end': 'parent',
-                'fn': function($el,pcnt) {
-                    var $spans = $el.find('span');
-                    var point = Math.floor(($spans.length+1) * pcnt);
-                    $spans.each(function(i,el) {
-                        var $span = $(el);
-                        if (i < point) {
-                            $span.addClass('visible');
-                        } else {
-                            $span.removeClass('visible');
-                        }
-                    });
-                }
-            }
-        ]);
+	});
 })
